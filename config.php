b12d0b2e7e683a4d9da71d8bf129f185c07ce7b5
Cesar dos Santos de Almeida <cesarsantosss499@gmail.com>
2021-12-04 15:20:29 -0300

    incio da documentação

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("BASE_URL", "http://localhost/api-frases-rick-morty/");
-
-define("STATUS_SUCESSO", "Sucesso");
-define("STATUS_ERROR", "Error");
-define("STATUS_ATENCAO", "Atenção");
